868b8fdfb5898babcdd69278716ba3b1a465d7c0
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-01-04 21:29:43 +0600

    added some content

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     errorElement:<h1>hello error</h1>,
-    element: <App></App>,
+    element: <Home></Home>,
     children: [
       {
         path: '/',
-        element: <h1>home</h1>
+        element: <Home></Home>
       },
       {
-        path: 'orders',
-        element: <h1>Orders</h1>,
+        path: 'about',
+        element: <About></About>
         // loader: cartProductsLoader
       },
       {
-        path: 'inventory',
-        element: <h2>inventory</h2>
+        path: 'contact',
+        element: <Contact></Contact>
       },
       {
         path:'checkout',
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router"
 
 
 const Home = () => {
   return (
     <div>
-      
+      <h1>Hello world</h1>
+      <Outlet></Outlet>
     </div>
   )
 }
