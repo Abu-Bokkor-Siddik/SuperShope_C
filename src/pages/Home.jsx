8eae8870e4e2d15d03394979cac5ae50879efdba
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-01-05 22:58:30 +0600

    added banner section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Hero from "../Components/Hero";
 
 const Home = () => {
   return (
     <div className="max-h-full min-w-96 ">
-      <h1>Home</h1>
+      <Hero></Hero>
+
     </div>
   );
 };
