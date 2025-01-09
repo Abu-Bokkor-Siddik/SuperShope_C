ebafeefd43d91520955cdc0adc1b62b571bf7f37
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-01-09 12:58:16 +0600

    added context api

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
 
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import { app } from "../../firebase"
 import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 export const AuthContext= createContext(null)
 const auth = getAuth(app)
 console.log(auth,'here auth')
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
     const [loading,setloading]=useState(true)
     const [user,setuser]=useState(null)
     // for data loading
@@ -15,7 +15,7 @@ const AuthProvider = () => {
 
  // register 
  const register = (email,password)=>{
-    // setloading(true)
+    setloading(true)
     return createUserWithEmailAndPassword(auth,email,password)
 }
  // login 
@@ -25,17 +25,43 @@ const AuthProvider = () => {
 }
   // logout 
   const logout = ()=>{
-    // setloading(true)
+    setloading(true)
     return signOut(auth)
 }
  // google
  const provider = new GoogleAuthProvider();
 
  const google =()=>{
-    //  setloading(true)
+     setloading(true)
      return signInWithPopup(auth,provider)
  }
- const authInfo = {google,register,logout,logins}
+
+
+  // onauthState 
+  useEffect(()=>{
+    const subricribe = onAuthStateChanged(auth, (currect) => {
+        // const logeruser = {email:currect?.email}
+        setuser(currect)
+        setloading(false)
+        // console.log('user ',logeruser)
+        // if(currect){
+           
+        //     axios.post('https://assignment-pink-eight.vercel.app/jwt1',logeruser,{withCredentials:true})
+        //     .then(res => {
+        //       console.log(res.data)})
+        // }else{
+        //     axios.post('https://assignment-pink-eight.vercel.app/logout',logeruser,{withCredentials:true})
+    
+        //     .then(res => {
+        //       console.log(res.data)})
+          
+        //    }
+    })
+    return ()=>{
+        subricribe()
+    }
+},[])
+ const authInfo = {google,register,logout,logins,user,loading,setloading}
   return (
     <div>
       AuthProvider
